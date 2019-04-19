4748932cd9f2d8d290f210a04ab4aadfa708944a
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-04-19 00:40:10 -0400

    log.txt addded, .js started

--- keys.js
+++ keys.js
@@ -4,3 +4,31 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+var fs = require("fs");
+
+var axios = require("axios");
+
+var action = process.argv[2];
+
+switch (action) {
+
+  case "concert-this":
+    
+    var bandName = [];
+
+    for (var b = 3; b < process.argv.length; b++) {
+      bandName.push(process.argv[b]);
+      console.log(bandName);
+    }
+    
+    var bandsUrl = "//rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(bandsUrl).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+
+
+}
--- liri.js
+++ liri.js
@@ -3,3 +3,11 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+action = process.argv[2];
+
+switch (action) {
+    
+    case "concert-this": 
+        var bandName = process.argv[3];
+}
